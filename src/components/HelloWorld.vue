0e47895e2fbec20ee4eba4ee4fc4ccef42c17128
jotacarvalh0 <josecarvalho11235@gmail.com>
2023-09-27 22:35:27 -0300

    CREAT: First Commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="greetings">
+    <label>Insira sua pergunta</label>
+    <input type="text" v-model="question"/>
+    <p>{{ respostaFinal }}</p>
+    <button @click="gerarResposta">Perguntar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+      return{
+        question: "",
+        respostaFinal: "",
+        answer: [
+          'Com certeza',
+          'É... não tenho certeza!',
+          'Claro que não',
+          'Acho difícil',
+          'Possívelmente não']
+      }
+    },
+    methods: {
+      gerarResposta() {
+          const respostaIndex = Math.floor(Math.random() * 5);
+          this.respostaFinal = this.answer[respostaIndex];
+      }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
